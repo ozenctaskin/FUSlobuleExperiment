2eedd6a2e0e7066d9c0c6972d2b174cf6dbf7d49
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2024-12-31 01:59:58 -0500

    Get rid of the stuff we don't need in the experiment script

--- scripts/run_FUSblock.m
+++ scripts/run_FUSblock.m
@@ -7,8 +7,10 @@ global NeuroFUS
 
 %% Set fix parameters.
 % Warning. Power below is the globalPower we want to use along with sham
-% power which is set to 1. Trials are randomized. See line 71. 
-numTrials = 20;
+% power which is set to 1. The script asks whether you need real or sham.
+% If sham is selected, globalPower is 1, otherwise globalPower=Power. See
+% line 34.
+numTrials = 15;
 Power = 30000;
 SonicDuration = 500000 ; %in microseconds
 xdrCenterFreq = 500000;   % in hertz
@@ -19,25 +21,13 @@ burstLength = 300;   %  in microseconds( 10us resolution )
 filePath = matlab.desktop.editor.getActiveFilename;
 addpath(fullfile(fileparts(filePath), 'subfunctions'))
 
-%% Get subject info by asking an d create folders in Ozzy's dir
-saveDir = 'C:\Ozzy\lobuleExperiment\data';
-subjectID = input('Enter subject ID: ', 's');
-subjectFolder = fullfile(saveDir, subjectID);
-if ~isfolder(subjectFolder)
-    mkdir(subjectFolder)
-end
-
-% Get stim location
-locationID = input('Enter the number for stimulated region \n1-Dentate \n2-Lubule 8 \n3-Lobule 5 : ', 's');
-if strcmp(locationID, '1')
-    fileName = 'Dentate';
-elseif strcmp(locationID, '2')
-    fileName = 'Lubule 8';
-elseif strcmp(locationID, '3')
-    fileName = 'Lubule 5';
-else
-    error('Number not recognized enter 1 for dentate, 2 for lobule8, or 3 for lobule5') 
-end
+% %% Get subject info by asking and create folders in Ozzy's dir
+% saveDir = 'C:\Ozzy\lobuleExperiment\data';
+% subjectID = input('Enter subject ID: ', 's');
+% subjectFolder = fullfile(saveDir, subjectID);
+% if ~isfolder(subjectFolder)
+%     mkdir(subjectFolder)
+% end
 
 % Get the depth measurement for the condition
 Depth = input('\nEnter the depth measurement from modelling for this region: ');
@@ -52,10 +42,6 @@ else
     error('Enter r for real, s for sham') 
 end
 
-% Get today's date time
-x = datetime(now,'ConvertFrom','datenum');
-fileTime = [num2str(x.Day), '-', num2str(x.Month), '-', num2str(x.Year), '_', num2str(x.Hour) num2str(x.Minute)];
-
 %% Connect to TPO
 disp('\nConnecting to TPO....');
 NFOpen('COM8',1,1);
@@ -67,7 +53,7 @@ NFBurstLength(NeuroFUS,burstLength);
 NFPulseRepPeriod(NeuroFUS,PRP);
 NFDuration(NeuroFUS,SonicDuration);
 NFDepth(NeuroFUS,Depth);
-pause(2)
+pause(3)
 
 %% START Treatment
 for ii = 1:numTrials
@@ -77,6 +63,3 @@ for ii = 1:numTrials
     NFStop(NeuroFUS);
     pause(4)
 end
-
-% Save trial order
-save(fullfile(subjectFolder, ['trialOrder_', subjectID, '_' fileName, '_', fileTime]), 'trials') 
