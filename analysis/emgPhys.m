ce22367f9cc77fab78b5a2a3e4130a48e5093804
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-01-04 05:04:30 -0500

    Adding more plotting options

--- analysis/emgPhys.m
+++ analysis/emgPhys.m
@@ -46,24 +46,35 @@ end
 combinedData = cellfun(@(row) vertcat(row{:}), num2cell(dataAll, 2), 'UniformOutput', false);
 combinedTUSevents = cellfun(@(row) vertcat(row{:}), num2cell(TUSevents, 2), 'UniformOutput', false);
 
+% %%% DELET THIS
+combinedData{1} = combinedData{1}(1:100000);
+combinedData{2} = combinedData{2}(1:100000);
+combinedTUSevents{1} = combinedTUSevents{1}(1:100000);
+combinedTUSevents{2} = combinedTUSevents{2}(1:100000);
+
 % Set a time vector and bandpass values
 t = (1:size(combinedData{1},1))/fs;
-bandpass = [5 100];
+bandpass = [20 150];
 
 %% Plot average measurements from trials
 % Set figures
-ax1 = subplot(1,2,1);
-ax2 = subplot(1,2,2);
+ax1 = subplot(1,3,1);
+ax2 = subplot(1,3,2);
+ax3 = subplot(1,3,3);
 hold(ax1, 'on');
 hold(ax2, 'on');
+hold(ax3, 'on');
 xlabel(ax1, 'Frequency'); ylabel(ax1, 'power (dB/Hz)');
 xlabel(ax2, 'Frequency'); ylabel(ax2, 'power (db/Hz)');
+xlabel(ax3, 'Condition'); ylabel(ax3, 'Exponent');
 xlim(ax1, bandpass)
 xlim(ax2, bandpass)
+xlim(ax3, [0, 3])
 title(ax1, 'Non-oscillatory');
 title(ax2, 'Oscillatory');
-colors = [[0.9290 0.6940 0.1250], 'r'];
-sgtitle(['Trial average'])
+title(ax3, 'Exponent of non-oscillatory components');
+colors = ['b', 'r'];
+sgtitle('Trial average')
 
 % Time frequency cell for comparison later
 TFA = {};
@@ -103,9 +114,13 @@ for ii = 1:size(combinedData,1)
     cfg.pad           = 'nextpow2';
     cfg.method        = 'irasa';
     cfg.output        = 'fractal';
+    cfg.keeptrials    = 'no';
     fractal = ft_freqanalysis(cfg, data);
     cfg.output        = 'original';
     original = ft_freqanalysis(cfg, data);
+    % keep trials, do fractal again, so we can plot all in a single plt
+    cfg.keeptrials    = 'yes';
+    fractal_trials = ft_freqanalysis(cfg, data);
 
     % subtract the fractal component from the power spectrum
     cfg               = [];
@@ -116,21 +131,28 @@ for ii = 1:size(combinedData,1)
     % display the spectra in log-log scale
     plot(ax1, fractal.freq, log(fractal.powspctrm), colors(ii)); 
     plot(ax2, fractal.freq, log(oscillatory.powspctrm), colors(ii));
+
+    % Plot individual trial non-oscillatory
+    for trl = 1:size(fractal_trials.powspctrm,1)
+        p = polyfit(log(fractal.freq), log(reshape(fractal_trials.powspctrm(trl,1,:), 1, [])), 1);
+        scatter(ax3, ii, p(2), colors(ii))
+    end
+
 end
 legend(ax1, {'Real', 'Sham'});
 legend(ax2, {'Real', 'Sham'});
 
-% Plot TFA difference real - sham
-cfg = [];
-cfg.parameter = 'powspctrm'; % The parameter to operate on
-cfg.operation = 'x1 - x2';  % Subtraction operation
-TFRdiff = ft_math(cfg, TFA{1}, TFA{2});
-cfg = [];
-% cfg.zlim = 'maxabs';
-cfg.channel = 'EMG';
-cfg.showlabels = 'yes';
-cfg.trials = 'all';
-ft_singleplotTFR(cfg, TFRdiff);
+% % Plot TFA difference real - sham
+% cfg = [];
+% cfg.parameter = 'powspctrm'; % The parameter to operate on
+% cfg.operation = 'x1 - x2';  % Subtraction operation
+% TFRdiff = ft_math(cfg, TFA{1}, TFA{2});
+% cfg = [];
+% % cfg.zlim = 'maxabs';
+% cfg.channel = 'EMG';
+% cfg.showlabels = 'yes';
+% cfg.trials = 'all';
+% ft_singleplotTFR(cfg, TFRdiff);
 
 %% Just analyze the real data, compare FUS section to baseline
 for ii = 1:size(combinedTUSevents,1)
