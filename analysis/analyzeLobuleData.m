28c0fdd7bc48f58974958ab56f24ecd9337dda71
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-01-04 05:05:06 -0500

    Adding another subject

--- analysis/analyzeLobuleData.m
+++ analysis/analyzeLobuleData.m
@@ -1,13 +1,29 @@
-dentate = load('/Users/ozzy/Desktop/ses2/Dentate_161224_000.mat');
-dentateSham0w = load('/Users/ozzy/Desktop/ses2/DentateSham0w_161224_000.mat');
-dentateSham30w = load('/Users/ozzy/Desktop/ses2/DentateSham302_161224_000.mat');
-lobule5 = load('/Users/ozzy/Desktop/ses2/Lobule5_161224_000.mat');
-lobule8 = load('/Users/ozzy/Desktop/ses2/Lobule8_161224_000.mat');
-V1 = load('/Users/ozzy/Desktop/ses2/V1_161224_000.mat');
-baseline = load('/Users/ozzy/Desktop/ses2/X_161224_000.mat');
+% % Sumayya
+% dentate = load('/Users/ozzy/Desktop/ses2/Dentate_161224_000.mat');
+% dentateSham0w = load('/Users/ozzy/Desktop/ses2/DentateSham0w_161224_000.mat');
+% dentateSham30w = load('/Users/ozzy/Desktop/ses2/DentateSham302_161224_000.mat');
+% lobule5 = load('/Users/ozzy/Desktop/ses2/Lobule5_161224_000.mat');
+% lobule8 = load('/Users/ozzy/Desktop/ses2/Lobule8_161224_000.mat');
+% V1 = load('/Users/ozzy/Desktop/ses2/V1_161224_000.mat');
+% baseline = load('/Users/ozzy/Desktop/ses2/X_161224_000.mat');
 
-data = {baseline, dentate, lobule5, lobule8, V1, dentateSham0w, dentateSham30w};
-labels = {'baseline','dentate','lobule5','lobule8','V1sham','sham0w','sham30w'};
+% NK
+dentate = load('/Users/ozzy/Desktop/HERO_03dat/DN_030125_000.mat');
+dentateSham0w = load('/Users/ozzy/Desktop/HERO_03dat/DN_0w_030125_000.mat');
+dentateSham30w = load('/Users/ozzy/Desktop/HERO_03dat/Dn_30wflip_030125_000.mat');
+lobule5 = load('/Users/ozzy/Desktop/HERO_03dat/L5_030125_000.mat');
+lobule8 = load('/Users/ozzy/Desktop/HERO_03dat/L8_take2_030125_000.mat');
+V1 = load('/Users/ozzy/Desktop/HERO_03dat/V1_030125_000.mat');
+baseline = load('/Users/ozzy/Desktop/HERO_03dat/V1_030125_000.mat');
+
+% % SMY
+% data = {baseline, dentate, lobule5, lobule8, V1, dentateSham0w, dentateSham30w};
+% labels = {'Nobaseline','dentate','lobule5','lobule8','V1sham','sham0w','sham30w'};
+% allPeaks =[];
+
+%Nika
+data = {dentate, lobule5, lobule8, V1, dentateSham0w, dentateSham30w};
+labels = {'dentate','lobule5','lobule8','V1sham','sham0w','sham30w'};
 allPeaks =[];
 
 for ii = 1:length(data)
@@ -19,8 +35,8 @@ end
 
 boxplot(allPeaks, labels)
 
-x = [allPeaks(:,1) + allPeaks(:,6) + allPeaks(:,7)] ./ 3;
-allPeaks = allPeaks(:,1:5);
-allPeaks(:,1) = x;
-figure
-boxplot(allPeaks, {'baseline','dentate','lobule5','lobule8','V1sham'})
+% x = [allPeaks(:,1) + allPeaks(:,6) + allPeaks(:,7)] ./ 3;
+% allPeaks = allPeaks(:,1:5);
+% allPeaks(:,1) = x;
+% figure
+% boxplot(allPeaks, {'baseline','dentate','lobule5','lobule8','V1sham'})
