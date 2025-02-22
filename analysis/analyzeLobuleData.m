77bde04424cfdc7b2f992b349cfad175b3c59352
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-02-22 02:58:46 -0500

    Plots and CBI ratios

--- analysis/analyzeLobuleData.m
+++ analysis/analyzeLobuleData.m
@@ -3,6 +3,7 @@ close all; clear all; clc
 % Decide if we are saving diagnostic images. Takes a while to run when set
 % to true
 setDiagnostics = false; 
+cleaned = true;
 
 % Specify subject paths. Order needs to be baseline, DN, L5, L8, V1, DN_0w,
 % DN_30w
@@ -30,6 +31,13 @@ data.HERO03 = {'/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/
                '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_03/clean_DN_0w_030125_000.mat', ...
                '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_03/clean_Dn_30wflip_030125_000.mat'}; 
 
+data.HERO04 = {'/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_Baseline_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_DN2_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_L5_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_L8_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_V1_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_DN0w_220125_000.mat', ...
+               '/Volumes/chenshare/Ozzy_Taskin/pilot_data/lobuleExperiment/data/HERO_04/clean_DN_flip30w_220125_000.mat'}; 
 
 % Get fieldnames and empty cell for all subject peaks so we can do some
 % averaging at the end. Also specify order of the data entry
@@ -95,13 +103,39 @@ end
 
 % Plot the subject average box plot
 averagePeaks = cat(1, averagePeaks{:});
+
+% Calculate ratio of paired vs. baseline
+ratios = averagePeaks ./ averagePeaks(:,1);
+averageRatios = mean(ratios);
+stdRatios = std(ratios);
+
+% Define specific colors for each subject
+colors = [1 0 0; 0 1 0; 1 0.4 0.8; 1 0.5 0]; 
+
+% Scatter plot with unique colors for each subject
 figure
 hold on
-for ii = 1:size(averagePeaks,2)
-    scatter(ii, averagePeaks(:,ii), 'b', 'filled', 'c', 'MarkerFaceColor', colors{ii})
+for ii = 1:size(averagePeaks,2) 
+    for jj = 1:numSubjects 
+        scatter(ii, averagePeaks(jj,ii), 50, colors(jj,:), 'filled'); 
+    end
 end
-xticklabels(labels)
-xlim([0 8])
-grid on
-title('Average')    
-    
+xlim([0 8]);
+ylim([0 3]);
+xticklabels(labels);
+legend(arrayfun(@(x) sprintf('HERO 0%d', x), 1:numSubjects, 'UniformOutput', false), ...
+    'Location', 'bestoutside'); % Add legend for subjects
+title('Subject results - Averaged Peaks');
+
+% Do a boxplot
+boxplot(averagePeaks, labels, 'Symbol', '')
+hold off;
+
+% for ii = 1:size(averagePeaks,2)
+%     scatter(ii, averagePeaks(:,ii), 'b', 'filled', 'c', 'MarkerFaceColor', colors{ii})
+% end
+% xticklabels(labels)
+% boxplot(averagePeaks, labels, 'Symbol', '')
+% xlim([0 8])
+% ylim([0 4])
+% title('Subject results - averaged peaks')
