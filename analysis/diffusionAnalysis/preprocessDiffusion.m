f86e1b1337f0d98fecef50eb7ca72a4f82b0a6ba
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-04-29 17:21:35 -0400

    Add tractography

--- analysis/diffusionAnalysis/preprocessDiffusion.m
+++ analysis/diffusionAnalysis/preprocessDiffusion.m
@@ -31,6 +31,18 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     if ~isfolder(dtiFolder)
         mkdir(dtiFolder)
     end  
+    anatProcess = fullfile(workdir, '06-anat');
+    if ~isfolder(anatProcess)
+        mkdir(anatProcess)
+    end  
+    subjectFod = fullfile(workdir, '07-subjectFOD_withOwnResponseFun');
+    if ~isfolder(subjectFod)
+        mkdir(subjectFod)
+    end  
+    subjectTractography = fullfile(workdir, '08-subjectTractography');
+    if ~isfolder(subjectTractography)
+        mkdir(subjectTractography)
+    end  
 
     % Get the input names
     dwiFolder = fullfile(dataFolder, subjectID, sessionID, 'dwi');
@@ -120,6 +132,39 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     md = fullfile(dtiFolder, 'md.mif');
     ad = fullfile(dtiFolder, 'ad.mif');
     rd = fullfile(dtiFolder, 'rd.mif');
-    system(['dwi2tensor -mask ' maskDilated ' ' upscaled_preprocessedImage ' ' dt]);
-    system(['tensor2metric -mask ' maskDilated ' -fa ' fa ' -adc ' md ' -ad ' ad ' -rd ' rd ' ' dt]);
+    % system(['dwi2tensor -mask ' maskDilated ' ' upscaled_preprocessedImage ' ' dt]);
+    % system(['tensor2metric -mask ' maskDilated ' -fa ' fa ' -adc ' md ' -ad ' ad ' -rd ' rd ' ' dt]);
+
+    % Perform a rigid registration between preprocessed dwi and anatomical
+    % and run 5ttgen tissue segmentation
+    anatomical = fullfile(dataFolder, subjectID, sessionID, 'anat', [subjectID '_' sessionID '_acq-btoMPRAGE2x11mmiso_T1w.nii.gz']);
+    singleVolDWI = fullfile(anatProcess, 'singleVolDWI.nii.gz');
+    % system(['mrconvert ' upscaled_preprocessedImage ' -coord 3 0 -axes 0,1,2 ' singleVolDWI]);
+    % system(['antsRegistrationSyN.sh -m ' singleVolDWI ' -f ' anatomical ' -t r -n 12 -o ' fullfile(anatProcess, 'dwi2anat')]);
+
+    registeredAnatomical = fullfile(anatProcess, 'anatInDWI.nii.gz');
+    genericAffine = fullfile(anatProcess, 'dwi2anat0GenericAffine.mat');
+    % system(['antsApplyTransforms -i ' anatomical ' -r ' anatomical ' -o ' registeredAnatomical ' -t [ ' genericAffine ',1 ]']);
+    
+    tissueSegments = fullfile(anatProcess, 'segmented5Tissues.mif');
+    % system(['5ttgen fsl ' registeredAnatomical ' ' tissueSegments ' -nthreads 12']); % ADD T2 here as well
+
+    % Calculate subject FOD image with own response function. Not to be
+    % used for fixel based analysis as we need a group FOD. Use dilated
+    % mask. For normalization, use the undilated mask.
+    wmFOD = fullfile(subjectFod, 'wm.mif'); wmFOD_norm = fullfile(subjectFod, 'wm_norm.mif');
+    gmFOD = fullfile(subjectFod, 'gm.mif'); gmFOD_norm = fullfile(subjectFod, 'gm_norm.mif');
+    csfFOD = fullfile(subjectFod, 'csf.mif'); csfFOD_norm = fullfile(subjectFod, 'csf_norm.mif');
+    % system(['dwi2fod msmt_csd ' upscaled_preprocessedImage ' ' wmResponse ' ' wmFOD ' ' gmResponse ' ' gmFOD ' ' csfResponse ' ' csfFOD ' -nthreads 12 -mask ' maskDilated]);
+    % system(['mtnormalise ' wmFOD ' ' wmFOD_norm ' ' gmFOD ' ' gmFOD_norm ' ' csfFOD ' ' csfFOD_norm ' -mask ' mask]);
+
+    % Calculate whole brain tractography and SIFT. We might want to go up 
+    % to 100 million streamlines. ADDD BIAS CORRECTION
+    % NECESSARY for SIFT!
+    seedGM = fullfile(subjectTractography, 'seedGM.mif');
+    wholeBrainTracts = fullfile(subjectTractography, 'wholeBrainTracts.tck');
+    wholeBrainTracts_SIFTED = fullfile(subjectTractography, 'wholeBrainTracts_SIFTED.tck');
+    system(['5tt2gmwmi ' tissueSegments ' ' seedGM]);
+    system(['tckgen -act ' tissueSegments ' -algorithm  Tensor_Prob -minlength 100 -select 5000 -nthreads 12 -seed_gmwmi ' seedGM ' ' upscaled_preprocessedImage ' ' wholeBrainTracts]);
+    system(['tcksift ' wholeBrainTracts ' ' wmFOD_norm ' ' wholeBrainTracts_SIFTED]);
 end
