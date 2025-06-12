23a130092622b12dcec1765e4e839f7d2f186f48
ozenc taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-06-12 05:15:12 -0400

    TMS/FUS analysis script overhaul and update

--- analysis/TMSAnalysis/analyzeLobuleData.m
+++ analysis/TMSAnalysis/analyzeLobuleData.m
@@ -1,10 +1,11 @@
 close all; clear all; clc
 
-% Decide if we are saving diagnostic images. Takes a while to run when set
-% to true
-setDiagnostics = false; 
-baselineCorrect = true;
-useClean = true;
+% Decide what this function will do when run
+setDiagnostics = false; % Saves diagnostic images in the data folder
+baselineCorrect = true; % Corrects baseline
+grandBaseline = true;   % Corrects a grand average baseline. Alternative is FUS corrected with FUS, TMS with TMS
+useClean = true;        % Use the clean data for analysis. Need to run cleanCBITrials first
+plotSubject = false;    % Plots all trials for all subjects in separate figure.
 
 % Specify subject paths. Order needs to be baseline, DN, L5, L8, V1, 0w,
 % 30wFlip
@@ -58,16 +59,18 @@ data.L001 =   {fullfile(dataFolder, 'L001_ses2', 'EMG', 'baseline_300525_000.mat
                fullfile(dataFolder, 'L001_ses2', 'EMG', 'V0W_300525_000.mat'), ...
                fullfile(dataFolder, 'L001_ses2', 'EMG', 'Flip_300525_000.mat'), ...
                fullfile(dataFolder, 'L001_ses2', 'EMG', 'CBI45_3005_1718_000.mat'), ...
-               fullfile(dataFolder, 'L001_ses2', 'EMG', 'CBI55_3005_1737_000.mat'), ...
-               fullfile(dataFolder, 'L001_ses2', 'EMG', 'CBI60_3005_1731_000.mat')}; 
+               fullfile(dataFolder, 'L001_ses2', 'EMG', 'CBI55_3005_1737_000.mat')}; 
+               % DROPPED fullfile(dataFolder, 'L001_ses2', 'EMG', 'CBI60_3005_1731_000.mat' 
+
 
 % Get fieldnames and empty cell for all subject peaks so we can do some
 % averaging at the end. Also specify order of the data entry
 subjectIDs = fieldnames(data);
-averagePeaks = {};
-labels = {'baseline','dentate','lobule 5','lobule 8','sham V1','sham 0w','sham 30w','CBI-1base','CBI-1','CBI-2base','CBI-2','CBI-3base','CBI-3'};
-labels = {'baseline','dentate','lobule 5','lobule 8','sham V1','sham 0w','sham 30w','baseline1','45-MSO','baseline2','55-MSO','baseline3','60-MSO'};
-colors = {'r', 'g', 'b', 'c', 'm', 'y', 'k', [0.8500, 0.3250, 0.0980], [0.8500, 0.3250, 0.0980], [0.4940, 0.1840, 0.5560], [0.4940, 0.1840, 0.5560], [0.3010, 0.7450, 0.9330], [0.3010, 0.7450, 0.9330]};
+allSubjectPeaks = {};
+% Set labels and colors. WARNING the word baseline needs to be used for
+% baseline labels, as the scripts look for it to find the baseline data
+labels = {'baseline','dentate','lobule 5','lobule 8','sham V1','sham 0w','sham 30w','baseline-1','CBI-1','baseline-2','CBI-2'};
+colors = {'r', 'g', 'b', 'c', 'm', 'y', 'k', [0.8500, 0.3250, 0.0980], [0.8500, 0.3250, 0.0980], [0.4940, 0.1840, 0.5560], [0.4940, 0.1840, 0.5560]};
 % Loop through sujects
 for sub = 1:length(subjectIDs)
     % Get the path of the subject from the first input and create a folder
@@ -79,6 +82,7 @@ for sub = 1:length(subjectIDs)
         end
     end
     % Load data into cells
+    data.(subjectIDs{sub}) = cellfun(@(p) fullfile(fileparts(p), ['clean_' extractAfter(p, [filesep 'EMG' filesep])]), data.(subjectIDs{sub}), 'UniformOutput', false);
     dataLoaded = cellfun(@(p) load_wave_data(p, useClean), data.(subjectIDs{sub}), 'UniformOutput', false);
     % Get an empty cell for subject peaks
     subjectPeaks = {};
@@ -95,16 +99,22 @@ for sub = 1:length(subjectIDs)
             testPeaks = peaks(find([dataset.frameinfo.state] == 1));
             condPeaks = peaks(find([dataset.frameinfo.state] == 2));
         end
-        
         % Here we do diagnostic plotting if asked
         if setDiagnostics
+            plotLabels = labels;
+            plotLabels(contains(plotLabels, 'baseline-')) = []; % drop baseline labels cuz we are plotting the entire CBI
             f = figure('visible','off');
-            sgtitle([subjectIDs{sub} ' ' labels{ii}])
+            sgtitle([subjectIDs{sub} ' ' plotLabels{ii}])
             for plt = 1:size(dataset.values, 3)
-                subplot(4,5,plt)
-                plot(dataset.values(2762:3100,1,plt))
+                if ii < 8
+                    subplot(4,5,plt)
+                    plot(dataset.values(2800:3100,1,plt))
+                else
+                    subplot(4,6,plt)
+                    plot(dataset.values(1025:end,1,plt))
+                end
             end
-            saveas(f,fullfile(dataPath, 'diagnostics', [subjectIDs{sub} '_' labels{ii}]),'jpg')
+            saveas(f,fullfile(dataPath, 'diagnostics', [subjectIDs{sub} '_' plotLabels{ii}]),'jpg')
         end
         % Check if the vector has less than 20 elements, if so add NaNs to
         % make it 20
@@ -132,80 +142,111 @@ for sub = 1:length(subjectIDs)
     end
     % Convert cell to matrix 
     subjectPeaks = cell2mat(subjectPeaks);
-    % Do a box plot for the values 
-    figure
-    boxplot(subjectPeaks, labels(1:size(subjectPeaks, 2)), 'Symbol', '')
-    ylim([0 6])
-    title(subjectIDs{sub})
-    hold on
-    % Add scatter points next to each boxplot
-    x = repmat(1:size(subjectPeaks, 2), size(subjectPeaks, 1), 1); % X-coordinates
-    x = x + 0.1 * (rand(size(x)) - 0.5); % Add a small random jitter to spread points
-    % x = x - 0.4;
-    for sct = 1:size(x,2)
-        scatter(x(:,sct), subjectPeaks(:,sct), 'filled', 'MarkerFaceAlpha', 0.6, 'MarkerFaceColor', colors{sct})
+    % Plot if requested 
+    if plotSubject
+        % Do a box plot for the values 
+        figure('Position', [100, 100, 800, 600])
+        boxplot(subjectPeaks, labels(1:size(subjectPeaks, 2)), 'Symbol', '')
+        ylim([0 6])
+        title(subjectIDs{sub})
+        hold on
+        % Add scatter points next to each boxplot
+        x = repmat(1:size(subjectPeaks, 2), size(subjectPeaks, 1), 1); % X-coordinates
+        x = x + 0.1 * (rand(size(x)) - 0.5); % Add a small random jitter to spread points
+        for sct = 1:size(x,2)
+            scatter(x(:,sct), subjectPeaks(:,sct), 'filled', 'MarkerFaceAlpha', 0.6, 'MarkerFaceColor', colors{sct})
+        end
+        hold off 
     end
-    hold off 
-
-    averagePeaks{sub} = nanmean(subjectPeaks);
+    allSubjectPeaks{sub} = subjectPeaks;
 end
 
-% Plot the subject average box plot
-averagePeaks = cat(1, averagePeaks{:});
+% Loop through all subject peak calculations and do averaging. Also do
+% baseline correction if requested
+baselineVals = find(contains(labels, 'baseline'));
+averageSubjectPeaks = {};
+for ii = 1:size(allSubjectPeaks,2)
+    % Average all trials
+    averageSubjectPeaks{ii} = nanmean(allSubjectPeaks{ii});
+    if baselineCorrect
+        if grandBaseline
+            % Find vectors based on "baseline" keyword in the labels cell.
+            baselines = allSubjectPeaks{ii}(:,baselineVals);
+            % Flatten and average baseline trials 
+            gBaseline = nanmean(baselines(:));
+            % Replace the first item on the cell (FUS baseline) with the
+            % grand baseline. Drop the rest of the baseline measurements
+            % from the averagePeaks.
+            averageSubjectPeaks{ii}(1) = gBaseline; 
+            averageSubjectPeaks{ii}(baselineVals(2:end)) = [];
+            % Now normalize each value with the baseline
+            averageSubjectPeaks{ii} = averageSubjectPeaks{ii}./averageSubjectPeaks{ii}(1);
+            averageSubjectPeaks{ii}(1) = [];
+        else
+            FUS = averageSubjectPeaks{ii}(1:7);
+            FUSbaselineCorrected = FUS./FUS(1);
+            FUSbaselineCorrected(1) = [];
+            TMS = averageSubjectPeaks{ii}(8:end);
+            TMSbaselineCorrected = TMS(2:2:end) ./ TMS(1:2:end);
+            averageSubjectPeaks{ii} = [];
+            averageSubjectPeaks{ii} = [FUSbaselineCorrected, TMSbaselineCorrected];
+        end
+    end
+end
 
-% Calculate ratio of paired vs. baseline
-ratios = averagePeaks ./ averagePeaks(:,1);
-averageRatios = mean(ratios);
-stdRatios = std(ratios);
-
-% % Define specific colors for each subject
-% colors = [1 0 0; 0 1 0; 1 0.4 0.8; 1 0.5 0; 0.2 0.4 0]; 
-
-numSubjects = 5; 
-
-% Baseline correct if asked
+% If baseline correct is passed, remove the baseline labels 
 if baselineCorrect
-    averagePeaks = averagePeaks./averagePeaks(:,1);
-    averagePeaks(:,1) = [];
-    labels = labels(2:end);
+    labels(baselineVals) = [];
 end
 
 % Do a boxplot
-figure
-boxplot(averagePeaks, labels, 'Symbol', '')
+subjectMat = cell2mat(averageSubjectPeaks');
+figure('Position', [100, 100, 800, 600])
+boxplot(subjectMat, labels, 'Symbol', '')
 hold on
 h = findobj(gca, 'Tag', 'Box'); 
 for j = 1:length(h)
     patch(get(h(j), 'XData'), get(h(j), 'YData'), [0.3010 0.7450 0.9330], 'FaceAlpha', 0.5); % Fill boxes with color
 end
 
-% Scatter plot with unique colors for each subject
-for ii = 1:size(averagePeaks,2) 
-    for jj = 1:numSubjects 
-        % scatter(ii, averagePeaks(jj,ii), 50, colors(jj,:), 'filled'); 
-        scatter(ii, averagePeaks(jj,ii), 50, 'k', 'filled'); 
+% Scatter plot
+for ii = 1:size(subjectMat,2) 
+    for jj = 1:numel(fieldnames(data))  
+        scatter(ii, subjectMat(jj,ii), 50, 'k', 'filled'); 
     end
 end
 
-% Set limits
+% Set plot properties
 if baselineCorrect
+    xlim([0 10]);
     ylim([0 6]);
-    xlim([0 7]);
-    ylabel('Baseline corrected MEP')
     yline(1, 'r--', 'LineWidth', 1.5);
+    if grandBaseline
+        ylabel('Grand baseline corrected MEP');
+    else
+        ylabel('Local baseline corrected MEP');
+    end
 else
     ylim([0 3]);
-    xlim([0 8]);
     ylabel('MEP amplitude (mV)')
 end
-
 xticklabels(labels);
 set(gca, 'FontSize', 25) 
 ax = gca;
 ax.Box = 'off';
 title('Subject results - Averaged Peaks');
 
-% Function to load data in without sub variable names
+% Save a sheet for future anayses. Only save if grandBaseline is used.
+if baselineCorrect && grandBaseline 
+    sheetLabels = [{'subject'}, labels];
+    subjects = fieldnames(data);
+    % Create table
+    T = array2table(subjectMat, 'VariableNames', sheetLabels(2:end));
+    T = addvars(T, subjects, 'Before', 1, 'NewVariableNames', sheetLabels{1});
+    writetable(T, fullfile(dataFolder, 'subject_results.xlsx'))
+end
+
+% Helper function to load data in without sub variable names
 function data = load_wave_data(filename, useClean)
     vars = whos('-file', filename);
     if useClean
