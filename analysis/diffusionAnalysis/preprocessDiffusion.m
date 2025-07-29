973211b6ecf4a54f6070bd98b5b571c8bf414501
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-07-29 15:50:18 -0400

    Change registration order

--- analysis/diffusionAnalysis/preprocessDiffusion.m
+++ analysis/diffusionAnalysis/preprocessDiffusion.m
@@ -1,6 +1,6 @@
 function preprocessDiffusion(dataFolder, subjectID, sessionID)
 
-    % Create a folder for the analysis 
+    % % Create a folder for the analysis 
     analysisFolder = fullfile(dataFolder, subjectID, sessionID, [subjectID '.diffusionResults']);
     if ~isfolder(analysisFolder)
         mkdir(analysisFolder)
@@ -41,11 +41,11 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     data = {AP98, PA98, AP99, PA99};
     bvecs = {AP98_bvec, PA98_bvec, AP99_bvec, PA99_bvec};
     bvals = {AP98_bval, PA98_bval, AP99_bval, PA99_bval};
-   
+
     % Convert to mif and do MP-PCA for each acquired image
     dataMPdenoised = {};
     for ii = 1:length(data)
-        
+
         % Get data path, name, and ext 
         [path, name, ext] = fileparts(data{ii});
 
@@ -77,8 +77,8 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     % Concatanate AP-PA pairs
     combinedDWI = fullfile(intermediateFiles, 'combinedDWI.mif'); 
     system(['mrcat ' APdiff ' ' PAdiff ' ' combinedDWI ' -axis 3']);
-    
-    % Pass it to preprocessing
+
+    % Pass it to FSL preprocessing and ants bias correction
     fslCorrectedDWI = fullfile(intermediateFiles, 'fslCorrectedDWI.mif');
     cleanDWI = fullfile(preprocessedResults, 'cleanDWI.mif');
     system(['dwifslpreproc ' combinedDWI ' ' fslCorrectedDWI ' -pe_dir AP -rpe_all -readout_time 0.0959097 -nthreads 12 -topup_options " --nthr=12 " ']);
@@ -95,7 +95,7 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     % Now upscale the cleaned image
     upscaledCleanDWI = fullfile(preprocessedResults, 'upscaledCleanDWI.mif');
     system(['mrgrid ' cleanDWI ' regrid -vox 1.25 ' upscaledCleanDWI])
-    
+
     % Create a whole brain mask from upscaled images. Provide a dilated
     % version as well since it's good for maps.
     mask = fullfile(preprocessedResults, 'mask.mif');
@@ -105,23 +105,28 @@ function preprocessDiffusion(dataFolder, subjectID, sessionID)
     system(['dwi2mask ' upscaledCleanDWI ' ' upscaledMask]);
     system(['maskfilter ' upscaledMask ' dilate -npass 2 ' upscaledMaskDilated]);
 
-    % Perform a rigid registration between preprocessed dwi and anatomical
-    % and run 5ttgen tissue segmentation
-    T1Image = fullfile(dataFolder, subjectID, sessionID, 'anat', [subjectID '_' sessionID '_acq-btoMPRAGE2x11mmiso_T1w.nii.gz']);
-    T2Image = fullfile(dataFolder, subjectID, sessionID, 'anat', [subjectID '_' sessionID '_acq-btoSPACET22x2CAIPI1mmiso_T2w.nii.gz']);
-    
+    % Register T1 to T2. Use DEOBLIQUED
+    T1Image = fullfile(dataFolder, subjectID, sessionID, 'anat', [subjectID '_' sessionID '_acq-btoMPRAGE2x11mmisoDEOBLIQUED_T1w.nii.gz']);
+    T2Image = fullfile(dataFolder, subjectID, sessionID, 'anat', [subjectID '_' sessionID '_acq-btoSPACET22x2CAIPI1mmisoDEOBLIQUED_T2w.nii.gz']);
+    system(['antsRegistrationSyN.sh -m ' T2Image ' -f ' T1Image ' -t r -n 12 -o ' fullfile(registrationsFolder, 'T2inT1')]);
+    T2inT1 = fullfile(registrationsFolder, 'T2inT1Warped.nii.gz');
+    T2inT1Affine = fullfile(registrationsFolder, 'T2inT10GenericAffine.mat');
+
+    % Register first volume of DWI to T1 image
     singleVolDWI = fullfile(intermediateFiles, 'singleVolDWI.nii.gz');
     system(['mrconvert ' upscaledCleanDWI ' -coord 3 0 -axes 0,1,2 ' singleVolDWI]);
     system(['antsRegistrationSyN.sh -m ' singleVolDWI ' -f ' T1Image ' -t r -n 12 -o ' fullfile(registrationsFolder, 'dwi2T1')]);
-    system(['antsRegistrationSyN.sh -m ' singleVolDWI ' -f ' T2Image ' -t r -n 12 -o ' fullfile(registrationsFolder, 'dwi2T2')]);
+    DWItoT1Affine = fullfile(registrationsFolder, 'dwi2T10GenericAffine.mat');
 
+    % Apply the inverse of DWI to T1 to anatomicals so that we get them in
+    % the DWI space. For T2, also add the affine matrix from T1-T2
+    % registration
     T1InDWI = fullfile(preprocessedResults, 'T1InDWI.nii.gz');
     T2InDWI = fullfile(preprocessedResults, 'T2InDWI.nii.gz');
-    genericAffineT1 = fullfile(registrationsFolder, 'dwi2T10GenericAffine.mat');
-    genericAffineT2 = fullfile(registrationsFolder, 'dwi2T20GenericAffine.mat');
-    system(['antsApplyTransforms -i ' T1Image ' -r ' T1Image ' -o ' T1InDWI ' -t [ ' genericAffineT1 ',1 ]']);
-    system(['antsApplyTransforms -i ' T2Image ' -r ' T2Image ' -o ' T2InDWI ' -t [ ' genericAffineT2 ',1 ]']);
+    system(['antsApplyTransforms -i ' T1Image ' -r ' T1Image ' -o ' T1InDWI ' -t [ ' DWItoT1Affine ',1 ]']);
+    system(['antsApplyTransforms -i ' T2Image ' -r ' T1Image ' -o ' T2InDWI ' -t [ ' DWItoT1Affine ',1 ]' ' -t [ ' T2inT1Affine ',0 ]']);
     
+    % Do a 5ttgen segmentation
     tissueSegments = fullfile(preprocessedResults, 'segmented5Tissues.mif');
     system(['5ttgen fsl ' T1InDWI ' ' tissueSegments ' -nthreads 12 -t2 ' T2InDWI]);
 end
