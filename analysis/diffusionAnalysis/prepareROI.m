1f835877c9bb2fe668341a61e8d8c9645eee8455
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-08-19 02:09:02 -0400

    Add freesurfer-mrtrix label conversion

--- analysis/diffusionAnalysis/prepareROI.m
+++ analysis/diffusionAnalysis/prepareROI.m
@@ -6,7 +6,7 @@ function prepareROI(dataFolder, subjectID, sessionID)
 
     % Run freesurfer
     surfDir = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], [subjectID '_freesurfer']);
-    if ~isfile(surfDir)
+    if ~isfolder(surfDir)
         system(['recon-all -all -subject ' subjectID '_freesurfer' ' -i ' T1w ' -T2 ' T2w ' -T2pial -sd ' fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'])]);
     end
 
@@ -14,6 +14,21 @@ function prepareROI(dataFolder, subjectID, sessionID)
     tsegments = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], 'preprocessed', 'segmented5Tissues.mif');
     if ~isfile(tsegments)
         system(['5ttgen hsvs -nthreads 10 -white_stem ' surfDir ' ' tsegments]);
-    end   
+    end
 
+    % Convert freesurfer labels
+    aparc_aseg = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], [subjectID, '_freesurfer'], 'mri', 'aparc+aseg.mgz');
+    colorLUT = fullfile(getenv('FREESURFER_HOME'), 'FreeSurferColorLUT.txt');
+    fsdefault_trix = '/home/chenlab-linux/mrtrix3/share/mrtrix3/labelconvert/fs_default.txt';
+    subjectNodes = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], 'preprocessed', 'subjectNodes.mif');
+    if ~isfile(subjectNodes)
+        system(['labelconvert ' aparc_aseg ' ' colorLUT ' ' fsdefault_trix ' ' subjectNodes])
+    end
+
+    % Replace subcortical regions with fsl fast regions. Give norm.mgz as
+    % the input as this is used in the hsvs processing to avoid any
+    % inconsistencies due to any interpolation.
+    norm_T1 = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], [subjectID, '_freesurfer'], 'mri', 'norm.mgz');
+    subjectNodes_gmFixed = fullfile(dataFolder, subjectID, sessionID, [subjectID, '.diffusionResults'], 'preprocessed', 'subjectNodes_gmFixed.mif');
+    system(['labelsgmfix ' subjectNodes ' ' norm_T1 ' ' fsdefault_trix ' ' subjectNodes_gmFixed ' -premasked']);
 end
