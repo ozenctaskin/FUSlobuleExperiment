58391dc54235e4ed180945382b76a26601e9af01
Ozzy <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2025-09-17 01:27:59 -0400

    Make average response function

---
+++ analysis/diffusionAnalysis/averageResponseFunctions.m
@@ -0,0 +1,33 @@
+function averageResponseFunctions(bidsFolder)
+
+    % Get folders that starts with sub-
+    d = dir(fullfile(bidsFolder, 'sub-*'));
+
+    % Keep only folders (exclude files) and append path
+    subFolders = d([d.isdir]);
+    subFolders = fullfile(bidsFolder, {subFolders.name});
+
+    % Loop through subjects and append response function paths to a strings
+    wmResponse = '';
+    gmResponse = '';
+    csfResponse = ''; 
+    subjectList = '';
+    for ii = 1:length(subFolders)
+        [~, subjectID] = fileparts(subFolders{ii});
+        if isfile(fullfile(subFolders{ii}, 'ses-01', [subjectID '.diffusionResults'], 'preprocessed', 'wmResponse.txt'))
+            wmResponse = [wmResponse fullfile(subFolders{ii}, 'ses-01', [subjectID '.diffusionResults'], 'preprocessed', 'wmResponse.txt') ' '];
+            gmResponse = [gmResponse fullfile(subFolders{ii}, 'ses-01', [subjectID '.diffusionResults'], 'preprocessed', 'gmResponse.txt') ' '];
+            csfResponse = [csfResponse fullfile(subFolders{ii}, 'ses-01', [subjectID '.diffusionResults'], 'preprocessed', 'csfResponse.txt') ' '];
+            subjectList = [subjectList subjectID ' '];
+        end
+    end
+
+    % Run response function averaging 
+    system(['responsemean ' wmResponse ' ' fullfile(bidsFolder, 'wmAverageResponse.txt')]);
+    system(['responsemean ' gmResponse ' ' fullfile(bidsFolder, 'gmAverageResponse.txt')]);
+    system(['responsemean ' csfResponse ' ' fullfile(bidsFolder, 'csfAverageResponse.txt')]);
+
+    % Write the list of subjects we used for response averaging to a text
+    % file in the bidsFolder
+    writelines(subjectList, fullfile(bidsFolder, 'responseSubjectList.txt'));
+end
